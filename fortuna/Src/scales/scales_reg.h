c241bbe5c44e1cd20ed16a226596ed40b2a1f12b
wkxboot <1131204425@qq.com>
2018-01-05 22:27:43 +0800

    编写完电子秤寄存器地址和数量。

--- fortuna/Src/scales/scales_reg.h
+++ fortuna/Src/scales/scales_reg.h
@@ -11,48 +11,80 @@
 
 
 /* -----------------------Master Defines -------------------------------------*/
-#define  M_DISCRETE_INPUT_START        0
-#define  M_DISCRETE_INPUT_NDISCRETES   1
-#define  M_COIL_START                  0
-#define  M_COIL_NCOILS                 8
-#define  M_REG_INPUT_START             0
-#define  M_REG_INPUT_NREGS             1
-#define  M_REG_HOLDING_START           (40001)
-#define  M_REG_HOLDING_NREGS           98
+#define  M_DISCRETE_INPUT_START                0
+#define  M_DISCRETE_INPUT_NDISCRETES           1
+#define  M_COIL_START                          0
+#define  M_COIL_NCOILS                         8
+#define  M_REG_INPUT_START                     0
+#define  M_REG_INPUT_NREGS                     1
+#define  M_REG_HOLDING_START                  (40001)
+#define  M_REG_HOLDING_NREGS                   98
 
 /*电子秤变送器寄存器地址*/
-#define  DEVICE_ADDR_REG_ADDR              (40001)
-#define  DEVICE_BAUDRATE_REG_ADDR          (40002)
-#define  DEVICE_FSM_TYPE_REG_ADDR          (40003)
-#define  DEVICE_PROTOCOL_TYPE_REG_ADDR     (40004)
-#define  DEVICE_RESPONSE_DELAY_REG_ADDR    (40005)
-#define  DEVICE_LOCK_REG_ADDR              (40006)
-#define  DEVICE_FIRMWARE_VERTION_REG_ADDR  (40007)
-#define  DEVICE_RESET_REG_ADDR             (40008)
-#define  DEVICE_LOCK_REG_ADDR              (40009)
+#define  DEVICE_ADDR_REG_ADDR                 (40001)
+#define  DEVICE_ADDR_REG_CNT                   1 
+#define  DEVICE_BAUDRATE_REG_ADDR             (40002)
+#define  DEVICE_BAUDRATE_REG_CNT               1
+#define  DEVICE_FSM_TYPE_REG_ADDR             (40003)
+#define  DEVICE_FSM_TYPE_REG_CNT               1
+#define  DEVICE_PROTOCOL_TYPE_REG_ADDR        (40004)
+#define  DEVICE_PROTOCOL_TYPE_REG_CNT          1
+#define  DEVICE_RESPONSE_DELAY_REG_ADDR       (40005)
+#define  DEVICE_RESPONSE_DELAY_REG_CNT         1
+#define  DEVICE_LOCK_REG_ADDR                 (40006)
+#define  DEVICE_LOCK_REG_CNT                   1
+#define  DEVICE_FIRMWARE_VERTION_REG_ADDR     (40007)
+#define  DEVICE_FIRMWARE_VERTION_REG_CNT       1
+#define  DEVICE_RESET_REG_ADDR                (40008)
+#define  DEVICE_RESET_REG_CNT                  1
+#define  DEVICE_LOCK_REG_ADDR                 (40009)
+#define  DEVICE_LOCK_REG_CNT                   1
 
-#define  DEVICE_MEASUREMENT_HI_REG_ADDR    (40031)
-#define  DEVICE_MEASUREMENT_LO_REG_ADDR    (40032)
-#define  DEVICE_ADC_SPEED_REG_ADDR         (40033)
-#define  DEVICE_DIRECTION_REG_ADDR         (40034)
-#define  DEVICE_SMOOTH_TYPE_REG_ADDR       (40035)
-#define  DEVICE_SMOOTH_STRENGTH_REG_ADDR   (40036)
-#define  DEVICE_ZERO_CODE_REG_ADDR         (40036)
-#define  DEVICE_ZERO_MEASUREMENT_REG_ADDR  (40036)
-#define  DEVICE_GAIN_CODE_REG_ADDR         (40036)
-#define  DEVICE_GAIN_CODE_REG_ADDR         (40036)
-#define  DEVICE_AD_ORIGIN_CODE_REG_ADDR    (40031)
+#define  DEVICE_MEASUREMENT_REG_ADDR          (40031)
+#define  DEVICE_MEASUREMENT_REG_CNT            2
+#define  DEVICE_ADC_SPEED_REG_ADDR            (40033)
+#define  DEVICE_ADC_SPEED_REG_CNT              1
+#define  DEVICE_DIRECTION_REG_ADDR            (40034)
+#define  DEVICE_DIRECTION_REG_CNT              1
+#define  DEVICE_SMOOTH_TYPE_REG_ADDR          (40035)
+#define  DEVICE_SMOOTH_TYPE_REG_CNT            1
+#define  DEVICE_SMOOTH_STRENGTH_REG_ADDR      (40036)
+#define  DEVICE_SMOOTH_STRENGTH_REG_CNT        1
+#define  DEVICE_ZERO_CODE_REG_ADDR            (40037)
+#define  DEVICE_ZERO_CODE_REG_CNT              2
+#define  DEVICE_ZERO_MEASUREMENT_HI_REG_ADDR  (40039)
+#define  DEVICE_ZERO_MEASUREMENT_LO_REG_CNT    2
+#define  DEVICE_SPAN_CODE_REG_ADDR            (40041)
+#define  DEVICE_SPAN_CODE_REG_CNT              2
+#define  DEVICE_SPAN_MEASUREMENT_REG_ADDR     (40043)
+#define  DEVICE_SPAN_MEASUREMENT_REG_CNT       2
+#define  DEVICE_AD_ORIGIN_CODE_REG_ADDR       (40045)
+#define  DEVICE_AD_ORIGIN_CODE_REG_CNT         2
 
-#define  DEVICE_GROSS_WEIGHT_REG_ADDR      (40032)
-#define  DEVICE_NET_WEIGHT_REG_ADDR        (40033)
-#define  DEVICE_TARE_WEIGHT_REG_ADDR       (40034)
-#define  DEVICE_MAX_WEIGHT_REG_ADDR        (40035)
-#define  DEVICE_DIVISION_REG_ADDR          (40036)
-
-#define  DEVICE_ZERO_CALIBRATE_HI_REG_ADDR (40032)
-#define  DEVICE_ZERO_CALIBRATE_LO_REG_ADDR (40032)
-#define  DEVICE_GAIN_CALIBRATE_HI_REG_ADDR (40033)
-#define  DEVICE_GAIN_CALIBRATE_LO_REG_ADDR (40033)
+#define  DEVICE_GROSS_WEIGHT_REG_ADDR         (40081)
+#define  DEVICE_GROSS_WEIGHT_REG_CNT           2
+#define  DEVICE_NET_WEIGHT_REG_ADDR           (40083)
+#define  DEVICE_NET_WEIGHT_REG_CNT             2
+#define  DEVICE_TARE_WEIGHT_REG_ADDR          (40085)
+#define  DEVICE_TARE_WEIGHT_REG_CNT            2
+#define  DEVICE_MAX_WEIGHT_REG_ADDR           (40087)
+#define  DEVICE_MAX_WEIGHT_REG_CNT             2
+#define  DEVICE_DIVISION_REG_ADDR             (40089)
+#define  DEVICE_DIVISION_REG_CNT               1
+#define  DEVICE_ZERO_CALIBRATE_REG_ADDR       (40090)
+#define  DEVICE_ZERO_CALIBRATE_REG_CNT         2
+#define  DEVICE_SPAN_CALIBRATE_REG_ADDR       (40092)
+#define  DEVICE_SPAN_CALIBRATE_REG_CNT         2
+#define  DEVICE_MANUALLY_CLEAR_RANGE_REG_ADDR (40094)
+#define  DEVICE_MANUALLY_CLEAR_RANGE_REG_CNT   1
+#define  DEVICE_MANUALLY_CLEAR_REG_ADDR       (40095)
+#define  DEVICE_MANUALLY_CLEAR_REG_CNT         1
+#define  DEVICE_PWR_ON_CLEAR_REG_ADDR         (40096)
+#define  DEVICE_PWR_ON_CLEAR_REG_CNT           1
+#define  DEVICE_AUTO_CLEAR_RANGE_REG_ADDR     (40097)
+#define  DEVICE_AUTO_CLEAR_RANGE_REG_CNT       1
+#define  DEVICE_AUTO_CLEAR_TIME_REG_ADDR      (40098)
+#define  DEVICE_AUTO_CLEAR_TIME_REG_CNT        1
 
 
 
